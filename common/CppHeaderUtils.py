6a64d2298e8d6b08adc73b908b49b4b9c63c652a
zmark <351479188@qq.com>
2024-07-12 15:17:22 +0800

    refactor: CppHeaderUtils.py

--- common/CppHeaderUtils.py
+++ common/CppHeaderUtils.py
@@ -10,6 +10,46 @@ def normalize_path(path):
     return os.path.normpath(path).replace('\\', '/')
 
 
+def convert_to_relative_path(repo_dir, headers) -> list:
+    """
+    获取头文件相对于仓库的路径
+    """
+    header_relative_paths = []
+    for header in headers:
+        header_relative_path = os.path.relpath(header, repo_dir)
+        header_relative_paths.append(header_relative_path)
+    return header_relative_paths
+
+
+def count_headers(abs_include_dirs):
+    """
+    计算给定目录列表中的头文件数量。
+
+    :param abs_include_dirs: 包含绝对路径的目录列表。
+    :return: 头文件数量。
+    """
+    def count_files_in_dir(directory):
+        """
+        计算目录中的文件数量，包括子目录中的文件。
+
+        :param directory: 目录路径。
+        :return: 文件数量。
+        """
+        return sum(len(files) for _, _, files in os.walk(directory))
+
+    count = 0
+    invalid_dirs = []
+    for dir in abs_include_dirs:
+        if os.path.isfile(dir):
+            count += 1
+        elif os.path.isdir(dir):
+            count += count_files_in_dir(dir)
+        else:
+            invalid_dirs.append(dir)
+
+    return count, invalid_dirs
+
+
 def find_headers(file_path, include_dirs):
     """
     递归查找给定C/C++源文件所包含的所有头文件。
@@ -61,48 +101,10 @@ def find_headers(file_path, include_dirs):
     parse_file(file_path)
     return headers
 
-
-def get_header_relative_path(repo_dir, headers) -> list:
-    """
-    获取头文件相对于仓库的路径
-    """
-    header_relative_paths = []
-    for header in headers:
-        header_relative_path = os.path.relpath(header, repo_dir)
-        header_relative_paths.append(header_relative_path)
-    return header_relative_paths
+# 参数处理
 
 
-def count_headers(abs_include_dirs):
-    """
-    计算给定目录列表中的头文件数量。
-
-    :param abs_include_dirs: 包含绝对路径的目录列表。
-    :return: 头文件数量。
-    """
-    def count_files_in_dir(directory):
-        """
-        计算目录中的文件数量，包括子目录中的文件。
-
-        :param directory: 目录路径。
-        :return: 文件数量。
-        """
-        return sum(len(files) for _, _, files in os.walk(directory))
-
-    count = 0
-    invalid_dirs = []
-    for dir in abs_include_dirs:
-        if os.path.isfile(dir):
-            count += 1
-        elif os.path.isdir(dir):
-            count += count_files_in_dir(dir)
-        else:
-            invalid_dirs.append(dir)
-
-    return count, invalid_dirs
-
-
-def get_relative_headers(repo_path, cpp_file_relative_path, include_dirs_relative_pahts):
+def param_process(repo_path, cpp_file_relative_path, include_dirs_relative_pahts):
     # 获取绝对路径
     cpp_file_path = os.path.join(repo_path, cpp_file_relative_path)
     include_dirs = [os.path.join(repo_path, dir)
@@ -111,9 +113,21 @@ def get_relative_headers(repo_path, cpp_file_relative_path, include_dirs_relativ
     # 归一化路径
     cpp_file_path = normalize_path(cpp_file_path)
     include_dirs = [normalize_path(dir) for dir in include_dirs]
+    return cpp_file_path, include_dirs
+
+
+def get_abs_headers(repo_path, cpp_file_relative_path, include_dirs_relative_pahts):
+    cpp_file_path, include_dirs = param_process(
+        repo_path, cpp_file_relative_path, include_dirs_relative_pahts)
 
     headers = find_headers(cpp_file_path, include_dirs)
-    headers = get_header_relative_path(repo_path, headers)
+    return headers
+
+
+def get_relative_headers(repo_path, cpp_file_relative_path, include_dirs_relative_pahts):
+    headers = get_abs_headers(
+        repo_path, cpp_file_relative_path, include_dirs_relative_pahts)
+    headers = convert_to_relative_path(repo_path, headers)
     return headers
 
 
@@ -133,18 +147,21 @@ def main():
         './include/linux/compiler_types.h'
     ]
 
+    cpp_file_path, include_dirs = param_process(
+        repo_path, cpp_file_relative_path, include_dirs_relative_pahts)
+
     headers = get_relative_headers(
         repo_path, cpp_file_relative_path, include_dirs_relative_pahts)
 
     for header in headers:
         print(header)
 
-    # print("=======================================================")
-    # all, invalid_dirs = count_headers(include_dirs)
-    # print(f"Total/filtered/invalid: {all}/{len(headers)}/{len(invalid_dirs)}")
-    # print("Invalid directories:")
-    # for dir in invalid_dirs:
-    #     print(dir)
+    print("=======================================================")
+    all, invalid_dirs = count_headers(include_dirs)
+    print(f"Total/filtered/invalid: {all}/{len(headers)}/{len(invalid_dirs)}")
+    print("Invalid directories:")
+    for dir in invalid_dirs:
+        print(dir)
 
 
 if __name__ == "__main__":
