51bd08afb5228f3b49131b33a1e2907176b11c20
zmark <351479188@qq.com>
2024-07-24 20:17:49 +0800

    fix(PrintUtils): print_sep中参数len和内置len重复

--- common/PrintUtils.py
+++ common/PrintUtils.py
@@ -1,4 +1,4 @@
-def print_sep(title: str, len=80, sep="="):
+def print_sep(title: str, length=80, sep="="):
     """
     带有分隔符的打印，确定打印一行的长度
     :param title: 标题
@@ -6,6 +6,6 @@ def print_sep(title: str, len=80, sep="="):
     :param sep: 分隔符
     """
     len_title = len(title)
-    len_left = (len - len_title) // 2
-    len_right = len - len_title - len_left
+    len_left = (length - len_title) // 2
+    len_right = length - len_title - len_left
     print(f"{sep * len_left}{title}{sep * len_right}")
