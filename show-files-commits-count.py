6672bbf431c5a93a623c2ef30e3c3cca4b36bb64
zmark <351479188@qq.com>
2024-07-24 09:53:06 +0800

    feat: show-files-commits-count

---
+++ show-files-commits-count.py
@@ -0,0 +1,8 @@
+from common.GitUtils import count_files_commits
+
+if __name__ == '__main__':
+    repo_path = r'D:\coding\zhurong-CodeWisdom\test_codes\linux-stable\linux-stable'
+    cpp_file_relative_paths = ['mm/memory.c', 'mm/hugetlb.c']  # 需要解析的C/CPP文件路径
+
+    commits = count_files_commits(repo_path, cpp_file_relative_paths)
+    print(commits)
