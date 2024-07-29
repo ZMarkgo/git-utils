5487ca02be455e152dd3edc7e244154408bde1bc
zmark <351479188@qq.com>
2024-07-29 14:55:24 +0800

    perf(GitFilesFilter): 使用print_sep

--- common/GitFilesFilter.py
+++ common/GitFilesFilter.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 from common.GitUtils import copy_dir, remove_dir, list_gitignore_files, show_repo_size_info, remove_all_git_remotes, add_virtual_remote, create_branch, show_commit_count, show_earliest_commit_time
 from common.TimeUtils import Timer
+from common.PrintUtils import print_sep
 
 
 def split_files(original_repo_path="", target_paths: list = [],
@@ -11,6 +12,7 @@ def split_files(original_repo_path="", target_paths: list = [],
                 timer: Timer = None):
     if timer:
         timer.lap()
+    print_sep("参数检查")
     # 检查原始仓库是否存在
     if not os.path.isdir(os.path.join(original_repo_path, ".git")):
         print(
@@ -35,7 +37,7 @@ def split_files(original_repo_path="", target_paths: list = [],
     if timer:
         timer.lap_and_show("Check parameters")
 
-    print("========================复制仓库========================")
+    print_sep("复制仓库")
     # 复制原始仓库到新的位置
     new_repo_path = os.path.join(new_repo_location, new_repo_name)
     print(f"Original repo path: {original_repo_path}")
@@ -48,7 +50,7 @@ def split_files(original_repo_path="", target_paths: list = [],
     if timer:
         timer.lap_and_show("Copy repo")
 
-    print("========================提取文件及其历史========================")
+    print_sep("提取文件及其历史")
     # 使用 git filter-repo 提取指定文件的历史记录
     split_cmd = ['git', 'filter-repo']
     for target_file in target_paths:
@@ -65,7 +67,7 @@ def split_files(original_repo_path="", target_paths: list = [],
         timer.lap_and_show("Extract files and history")
 
     # 仓库瘦身
-    print("========================仓库瘦身========================")
+    print_sep("仓库瘦身")
     print("Before:")
     show_repo_size_info()
     # 移除 filter-repo 残留数据
@@ -82,7 +84,7 @@ def split_files(original_repo_path="", target_paths: list = [],
         timer.lap_and_show("Slim repo")
 
     # 为新仓库添加虚拟远程仓库
-    print("========================添加虚拟远程仓库========================")
+    print_sep("添加虚拟远程仓库")
     remove_all_git_remotes()
     add_virtual_remote(new_repo_name)
 
@@ -90,14 +92,14 @@ def split_files(original_repo_path="", target_paths: list = [],
         timer.lap_and_show("Add virtual remote")
 
     # 创建新分支
-    print("========================创建新分支========================")
+    print_sep("创建新分支")
     print(f"New branch name: {new_branch_name}")
     create_branch(new_branch_name)
 
     if timer:
         timer.lap_and_show("Create new branch")
 
-    print("========================处理完成========================")
+    print_sep("处理完成")
     # 展示提交数
     show_commit_count()
     # 展示最早的提交时间
