75aa3a50bf2839a2b267d8229ba480da8bb75970
zmark <351479188@qq.com>
2024-07-30 15:41:38 +0800

    feat(show-files-commits-count): 本地wsl测试

--- show-files-commits-count.py
+++ show-files-commits-count.py
@@ -1,7 +1,7 @@
 from common.GitUtils import count_files_commits
 
 if __name__ == '__main__':
-    repo_path = r'D:\coding\zhurong-CodeWisdom\test_codes\linux-stable\linux-stable'
+    repo_path = r'/mnt/d/coding/zhurong-CodeWisdom/test_codes/linux-stable/linux-stable'
     cpp_file_relative_paths = ['mm/memory.c', 'mm/hugetlb.c']  # 需要解析的C/CPP文件路径
 
     commits = count_files_commits(repo_path, cpp_file_relative_paths)
