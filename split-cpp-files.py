bf3852fa425ecd9c6adfb0a4835b2939c328542a
zmark <351479188@qq.com>
2024-08-13 16:20:56 +0800

    perf(spllit-cpp-files): 调用statistics_split_info

--- split-cpp-files.py
+++ split-cpp-files.py
@@ -1,7 +1,8 @@
 from common.TimeUtils import Timer
-from common.GitFilesFilter import split_files
+from common.GitFilesFilter import split_files, statistics_split_info
 from common.CppHeaderUtils import get_relative_headers_of_files, get_relative_headers_of_files_all_commits
 import traceback
+
 
 def main():
     timer = Timer()
@@ -20,6 +21,7 @@ def main():
         './include/linux/compiler_types.h'
     ]
     target_paths = ['mm/memory-failure.c']
+    target_c_files = target_paths.copy()
     new_repo_name = 'linux-split-memory-failure-careful-headers-all-gitignore-2'
     new_repo_location = r"/home/app/repository"
     new_branch_name = 'demo'
@@ -45,6 +47,8 @@ def main():
                     track_gitignore=track_gitignore,
                     timer=timer,
                     regex_with_glob=False)
+
+        statistics_split_info(repo_path, target_c_files, timer=timer)
     except Exception as e:
         print(f"Error: {e}")
         traceback.print_exc()
