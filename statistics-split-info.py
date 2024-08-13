0556a6f483d32bd2b8ad858c67116398a509622d
zmark <351479188@qq.com>
2024-08-13 16:04:00 +0800

    refactor(statistics-split-info): 重构

--- statistics-split-info.py
+++ statistics-split-info.py
@@ -4,15 +4,9 @@ from common.TimeUtils import Timer
 from common.PrintUtils import print_sep
 import argparse
 
-if __name__ == '__main__':
-    timer = Timer()
-    parser = argparse.ArgumentParser(
-        description='Show the earliest commit time of a Git repository.')
-    parser.add_argument('-r', '--repo', required=True,
-                        help='The path to the Git repository.')
-    args = parser.parse_args()
-    repo_path = args.repo
 
+def statistics_split_info(repo_path, cpp_file_relative_paths):
+    timer = Timer()
     print_sep('count files')
     timer.lap()
     show_count_file_ext(repo_path, '.c')
@@ -26,7 +20,6 @@ if __name__ == '__main__':
 
     print_sep('count commits of target files')
     timer.lap()
-    cpp_file_relative_paths = ['mm/memory-failure.c']
     show_count_files_commits(repo_path, cpp_file_relative_paths)
     timer.lap_and_show('Counting commits of target files')
 
@@ -37,3 +30,18 @@ if __name__ == '__main__':
 
     timer.end()
     timer.show_time_cost()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Show the earliest commit time of a Git repository.')
+    parser.add_argument('-r', '--repo', required=True,
+                        help='The path to the Git repository.')
+    parser.add_argument('-tf', '--target_files', nargs='+',
+                        required=True, help='The target files to be counted.')
+    args = parser.parse_args()
+    repo_path = args.repo
+    cpp_file_relative_paths = args.target_files
+    print('repo_path:', repo_path)
+    print('cpp_file_relative_paths:', cpp_file_relative_paths)
+    statistics_split_info(repo_path, cpp_file_relative_paths)
