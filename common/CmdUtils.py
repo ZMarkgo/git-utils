a470dfd9ecfca58d9c2975c4b357d813bd399d54
zmark <351479188@qq.com>
2024-08-16 11:11:25 +0800

    perf(CmdUtils): 新增参数shell

--- common/CmdUtils.py
+++ common/CmdUtils.py
@@ -1,14 +1,15 @@
 import subprocess
 
 
-def run_cmd(cmd, stdout_handler, stderr_handler, check=True):
+def run_cmd(cmd, stdout_handler, stderr_handler, check=True, shell=False):
     """
     运行命令并实时处理标准输出和标准错误。
 
-    :param cmd: 要运行的命令（以列表形式传递）
+    :param cmd: 要运行的命令 (以列表形式传递)
     :param stdout_handler: 处理标准输出的函数
     :param stderr_handler: 处理标准错误的函数
-    :param check: 是否检查命令的返回码并抛出异常（默认为 True）
+    :param check: 是否检查命令的返回码并抛出异常 (默认为 True)
+    :param shell: 是否使用 shell 运行命令 (默认为 False)
     """
     if stdout_handler is None:
         def stdout_handler(line): return None
@@ -18,7 +19,8 @@ def run_cmd(cmd, stdout_handler, stderr_handler, check=True):
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        text=True
+        text=True,
+        shell=shell
     ) as proc:
         # 实时处理标准输出
         for line in proc.stdout:
